b5968f21804ef9c33a76d2d2d39eaa3f432652d9
Favio Sauto <41118847+FavioSauto@users.noreply.github.com>
2021-01-21 10:42:30 -0400

    Initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,34 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: {url: '/', static: true},
+    src: {url: '/dist'},
+  },
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-typescript',
+    [
+      '@snowpack/plugin-build-script',
+      { cmd: 'postcss', input: ['.css'], output: ['.css'] },
+    ],
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
